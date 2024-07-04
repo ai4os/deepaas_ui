5a06cfeb8c5edcb16121019c29b47fc1c62328fd
Ignacio Heredia <iheredia@ifca.unican.es>
2024-07-04 17:28:05 +0200

    fix: update apt index

--- nomad.sh
+++ nomad.sh
@@ -5,6 +5,7 @@ DURATION="${DURATION:-10m}"
 UI_PORT="${UI_PORT:-8888}"
 
 # Processing video with Gradio requires ffmpeg
+apt-get update
 apt install ffmpeg --yes
 
 git clone -b nomad https://github.com/ai4os/deepaas_ui
